086c4a4b356e0b4e34b081c29b85d04fd4b82bce
Alen Suljkanović <alen.suljkanovic@typhoon-hil.com>
2018-05-17 13:30:30 +0200

    Add initial grammar

---
+++ cparser/__init__.py
@@ -0,0 +1,35 @@
+from parglare import Grammar
+from parglare import Parser
+
+grammar = Grammar.from_file("cgrammar.pg")
+parser = Parser(grammar)
+
+code = """
+
+union student 
+{
+   char name[20];
+   char subject[20];
+   float percentage;
+};
+
+struct Books {
+   char  title[50];
+   char  author[50];
+   char  subject[100];
+   int   book_id;
+} book;
+
+int main(int argc, const char **argv){
+
+   union student record1;
+
+   struct Books Book1;
+   Book1.book_id = 6495407;
+
+	return 0;
+}
+"""
+
+results = parser.parse(code)
+
