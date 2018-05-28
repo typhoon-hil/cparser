6b82ed3a73208f7f04030ecb01f249b68ad4462a
Alen Suljkanovic <alienized91@gmail.com>
2018-05-28 16:44:03 +0200

    Add disambiguation solvers and refactor code
    There were two productions in grammar that, in most cases, caused
    disambiguation. Those are:
    
    ```
    typedef_name:      ID;
    direct_declarator: ID;
    ```
    
    The problem is solved as follows. Sematic action is called for
    every `decl_body` production where the names user-defined types
    (struct, union, typedef) are collected. Dynamic filter is registered
    for the `typedef_name` production where we check if the `ID` that
    is trying to be reduced is in the collection of user-defined types.
    If so, then the reduction is performed. Otherwise, we reject the
    reduction.

--- tests/test_with_functions.py
+++ tests/test_with_functions.py
@@ -30,6 +30,7 @@ def test_with_functions(parser):
 
     results = parser.parse(code)
     print("Parsed {} trees.".format(len(results)))
+
     trees_count = min(len(results), 10)
     for tree in range(trees_count):
         print('Dumping tree {}'.format(tree))
