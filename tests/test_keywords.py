d4e189748805b727056594a1cf53eb619ea922a4
Alen Suljkanovic <alienized91@gmail.com>
2018-05-29 14:24:54 +0200

    Handle keyword ambiguity

---
+++ tests/test_keywords.py
@@ -0,0 +1,34 @@
+
+def test_recognizers(parser):
+    code = """
+    typedef int int8_t;
+
+    typedef int auto1;
+    typedef int register1;
+    typedef int static1;
+    typedef int external;
+    typedef int typedef1;
+    
+    typedef char character;
+    typedef short shorter;
+    typedef int integer;
+    typedef long longer;
+    typedef float floating;
+    typedef double doubler;
+    typedef signed signed1;
+    typedef unsigned unsigned1;
+    
+    typedef int constant;
+    typedef int volatilised;
+    
+    typedef int struct1;
+    typedef int union1;
+    
+    typedef int if1;
+    typedef int switch1;
+    
+    """
+
+    results = parser.parse(code)
+    print("Number of trees: {}".format(len(results)))
+    print(results[0].tree_str())
