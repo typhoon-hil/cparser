61a1ba318745078f139acf5ae3cd2f5f474bb086
Alen Suljkanović <alen.suljkanovic@typhoon-hil.com>
2018-06-15 15:57:37 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+import codecs
+from setuptools import setup, find_packages
+from cparser import get_version
+
+
+NAME = "cparser"
+AUTHOR = "Typhoon HIL, Inc"
+DESCRIPTION = "C Parser writen in pure Python"
+README = codecs.open(os.path.join(os.path.dirname(__file__), "README.rst"),
+                     "r", encoding="utf-8").read()
+
+setup(
+    name=NAME,
+    description=DESCRIPTION,
+    long_description=README,
+    version=get_version(),
+    license="MIT",
+    install_requires=["parglare"],
+    tests_require=["pytest"],
+    author="Typhoon HIL, Inc",
+    include_package_data=True,
+    packages=find_packages(),
+    keywords="parser C GLR",
+
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Topic :: Software Development",
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        "License :: OSI Approved :: MIT Licence",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ]
+)
