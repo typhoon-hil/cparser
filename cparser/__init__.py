67f32415ddac6b6ea6902f94c6ce16e4d63b0bdf
Alen Suljkanovic <alienized91@gmail.com>
2018-10-09 14:41:28 +0200

    Update version

--- cparser/__init__.py
+++ cparser/__init__.py
@@ -6,7 +6,7 @@ def get_version():
     Return C parser version. Used in setup.py and anywhere
     where version is needed.
     """
-    return "0.1"
+    return "0.1.1"
 
 
 __version__ = get_version()
