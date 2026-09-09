1b8a9dc48010c813a87c502817a29b574e412e26
borisav.zivanovic <borisav.zivanovic@typhoon-hil.com>
2026-09-09 16:46:13 +0200

    Redesigned CodeGenerator to emit line directives and newlines related to original code structure (fixes parsing problems that mix preprocessor and C syntax)

--- cparser/generator.py
+++ cparser/generator.py
@@ -1,35 +1,71 @@
 from cparser.visitor import ASTVisitor
+from parglare import pos_to_line_col
 
 
 class CodeGenerator(ASTVisitor):
     """Generates C code from the given AST"""
-    def __init__(self, keep_line_directives=False):
+    def __init__(
+        self,
+        emit_line_directives=False,
+        original_source_code='',
+        line_adjust=0,
+        line_file_prefix='',
+    ):
+        self.emit_line_directives = emit_line_directives
+        self.original_source_code = original_source_code
+        self.line_adjust = line_adjust
+        self.line_file_prefix = line_file_prefix
+
         self.indent = 0
-        self.keep_line_directives = keep_line_directives
+        self.current_line = 0
 
     def _add_indent(self):
         return " " * self.indent
 
-    def generate(self, ast, output_path=None, debug=False):
-        code = self.visit(ast, debug)
+    def generate(self, ast, output_path=None):
+        code = self.visit(ast)
         return code
 
-    def visit(self, node, debug=False):
-        visitor = "visit_%s" % node.symbol.name
+    def visit(self, node):
+        def _visit_internal(node):
+            visitor = "visit_%s" % node.symbol.name
 
-        if hasattr(self, visitor):
-            return getattr(self, visitor)(node)
-        else:
-            return self._generic_visit(node, debug)
+            if hasattr(self, visitor):
+                return getattr(self, visitor)(node)
+            else:
+                return self._generic_visit(node)
 
-    def _generic_visit(self, node, debug):
+        return self._wrap_emit_line(node, _visit_internal)
+
+    def _generic_visit(self, node):
         if not node:
             return ""
 
         if hasattr(node, "children") and node.children is not None:
             return " ".join(self.visit(c) for c in node.children)
         else:
-            return node.value
+            return self._wrap_emit_line(node, lambda n: n.value)
+
+    def _wrap_emit_line(self, node, callback):
+        code_result = callback(node)
+
+        emit_line_directive = False
+
+        if self.emit_line_directives:
+            node_line, node_col = pos_to_line_col(
+                self.original_source_code, node.context.start_position
+            )
+
+            adj_line = node_line - self.line_adjust
+
+            if adj_line > self.current_line:
+                self.current_line = adj_line
+                emit_line_directive = True
+
+        if emit_line_directive:
+            return f'\n# {self.current_line} "{self.line_file_prefix}"\n{code_result}'
+        else:
+            return code_result
 
     def visit_id(self, node):
         return node.value
@@ -78,7 +114,7 @@ class CodeGenerator(ASTVisitor):
 
     def visit_compound_stat(self, node):
         children = node.children
-        ret = "%s{\n" % self._add_indent()
+        ret = "%s{" % self._add_indent()
         self.indent += 4
 
         if len(children) == 3:
@@ -87,7 +123,7 @@ class CodeGenerator(ASTVisitor):
                            for b in block_item_list.children)
 
         self.indent -= 4
-        ret += "}\n"
+        ret += "}"
         return ret
 
     def visit_iteration_stat(self, node):
@@ -96,32 +132,21 @@ class CodeGenerator(ASTVisitor):
         if iter_name == "while":
             ret = "while ("
             ret += self.visit(node.children[2])  # visit exp
-            ret += ")\n"
+            ret += ")"
             ret += self._generate_stat(node.children[4])  # visit stat
             return ret
 
-        return self._generic_visit(node, True)
+        return self._generic_visit(node)
 
     def visit_jump_stat(self, node):
         ret = " ".join(self.visit(c) for c in node.children[:-1])
         return ret + ";"
 
     def visit_line_directive(self, node):
-        if self.keep_line_directives:
-            return f"\n# {node.children[1].value} {node.children[2].value}\n"
-
-        return "\n"
+        return ""
 
     def _generate_stat(self, node):
-
         if node.symbol.name == "compound_stat":
             return self.visit(node)
         else:
-            return self._add_indent() + self.visit(node) + "\n"
-
-
-
-
-
-
-
+            return self._add_indent() + self.visit(node)
